1d9da0993b6e7e88e6b9c59c4818fa673668194f
RITWIK SHARMA <91376984+Ritwik037@users.noreply.github.com>
2022-04-29 11:21:31 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,7 +1,7 @@
 def call(){
   sh 'docker build -t $docker_credentials_USR/sample:$BUILD_NUMBER .'
   sh 'docker login -u $docker_credentials_USR -P $docker_credentials_PSW'
-  SH 'docker push $docker_credentials_USR/sample:$BUILD_NUMBER'
+  sh 'docker push $docker_credentials_USR/sample:$BUILD_NUMBER'
 
 }  
 
