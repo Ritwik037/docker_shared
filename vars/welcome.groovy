bf71cfaeeb93f0059dbd96fd5eed7c6f981c61b6
RITWIK SHARMA <91376984+Ritwik037@users.noreply.github.com>
2022-04-29 17:01:42 +0530

    Update welcome.groovy

--- vars/welcome.groovy
+++ vars/welcome.groovy
@@ -1,7 +1,7 @@
 def call(map config = [:] ) {
-  sh 'docker build -t ${config.username}/sample:$BUILD_NUMBER .'
-  sh 'docker login -u ${config.username} -p ${config.password}'
-  sh 'docker push ${config.username}/sample:$BUILD_NUMBER'
+  sh 'docker build -t $${config.username}/sample:$BUILD_NUMBER .'
+  sh 'docker login -u $${config.username} -p $${config.password}'
+  sh 'docker push $${config.username}/sample:$BUILD_NUMBER'
   sh 'docker logout'
 
 }  
